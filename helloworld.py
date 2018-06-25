221fe77979571dfca9af738d3193e228b554e0a8
Conrad Corbett <conradcorbett@ccorbettca-mbp.lan>
2018-06-25 15:27:03 -0700

    commit

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,10 @@
 print "Hello World"
 
 for x in range(0, 3):
-    print "We're on time " % (x)
+    print "We're on time %d" % (x)
+
+
+check_list = [1, 2, 3, 4, 5]
+
+for n in check_list:
+    print "Number is %d" %n
